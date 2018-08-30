06efc2e6d23784b9b489d6cf1f29b63a714e7d7a
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-30 11:19:59 +0200

    Basic dashbaord.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def create_allergic_prefs
   end
 
@@ -6,5 +7,13 @@ class UsersController < ApplicationController
   end
 
   def delete_allergic_prefs
+  end
+
+  def dashboard
+    @user = current_user
+    @current_food_request = @user.food_requests.
+        where('created_at BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now).first
+    @user_historic_food_requests = @user.food_requests.
+        where('created_at  < ? ', DateTime.now.beginning_of_day)
   end
 end
